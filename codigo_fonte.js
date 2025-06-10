5ac1fb87f1193baf0e57670ab89c567abf4348a3
riqueivy <riqueivymeneses@gmail.com>
2025-06-10 14:03:44 -0300

    Update codigo_fonte.js

--- codigo_fonte.js
+++ codigo_fonte.js
@@ -2,39 +2,72 @@ const express = require('express');
 // importaçoes:
 const app = express();
 
-const tabelas = require('./config_API/tebelas');
+const tabelas = require('../config_API/tebelas.js');
+const conexao = require('../config_API/db.cjs');
 
-const conexao = require('./config_API/db.cjs');
+// Middleware para JSON
+app.use(express.json());
 
-const router = require('./routers.js');
+// Inicializa as tabelas
+tabelas.init(conexao);
 
-app.use = (express.json())
-
-//pagina de cadastro
-app.post('/usuarios', (req, res)=>{
-    const { nome, email, senha } = req.body;
-    const sql = 'INSERT INTO administracao (id_adm, login, senha) VALUES (?, ?, ?)';
-    
-    connection.query(sql, [nome, email, senha], (err, result) => {
-        if (err) throw err;
+// Página de cadastro
+app.post('/usuarios', (req, res) => {
+    const { email, senha } = req.body;
+    if (!email || !senha) {
+        return res.status(400).send('Email e senha são obrigatórios!');
+    }
+    const sql = 'INSERT INTO administracao (login, senha) VALUES (?, ?)';
+    conexao.query(sql, [email, senha], (err, result) => {
+        if (err) {
+            return res.status(500).send('Erro ao cadastrar usuário!');
+        }
         res.send('Usuário cadastrado com sucesso!');
     });
+});
 
-
-})
-
-
-
-//pagina de login
-app.get('/usuarios', (req, res)=>{
+// Página de login
+app.post('/login', (req, res) => {
     const { email, senha } = req.body;
+    if (!email || !senha) {
+        return res.status(400).send('Email e senha são obrigatórios!');
+    }
     const sql = 'SELECT * FROM administracao WHERE login = ? AND senha = ?';
+    conexao.query(sql, [email, senha], (err, result) => {
+        if (err) {
+            return res.status(500).send('Erro ao fazer login!');
+        }
+        if (result.length > 0) {
+            res.send('Login realizado com sucesso!');
+        } else {
+            res.status(401).send('Usuário ou senha inválidos!');
+        }
+    });
+});
 
-    res.send('ok')
-})
+// Consulta de aluno pelo QRCode
+app.get('/aluno', (req, res) => {
+    const { qrcode } = req.query;
+    if (!qrcode) {
+        return res.status(400).send('QRCode é obrigatório!');
+    }
+    const sql = 'SELECT * FROM aluno WHERE qrcode = ?';
+    conexao.query(sql, [qrcode], (err, result) => {
+        if (err) {
+            return res.status(500).send('Erro ao buscar aluno!');
+        }
+        res.send(result);
+    });
+});
 
-app.get('/usuarios', (req, res)=> {
-    res.send('bem vindo')
-})
+// Exemplo de rota principal
+app.get('/', (req, res) => {
+    res.send('Bem-vindo à API!');
+});
 
-app.listen(3000)
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
+
+
+
